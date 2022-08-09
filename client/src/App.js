513546be956544037287344f7918ecdff9c128bb
richxrd <linrichard98@gmail.com>
2022-08-09 13:54:33 -0400

    home and footer components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 const App = () => {
     return (
         <div>
             <Navbar />
+            <Home />
+            <Footer />
         </div>
     );
 };
