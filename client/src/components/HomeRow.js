e4553b45fa4e8a29829ff6c9174d51ad7e6ac625
richxrd <linrichard98@gmail.com>
2022-08-09 13:54:09 -0400

    home row

---
+++ client/src/components/HomeRow.js
@@ -0,0 +1,23 @@
+import React from "react";
+import CategoryCard from "./CategoryCard";
+
+const HomeRow = ({ category }) => {
+    return (
+        <div className="flex flex-col space-y-2">
+            {/* Title */}
+            <div>
+                <h1 className="text-2xl font-medium tracking-wide uppercase text-[#d9534f]">
+                    {category}
+                </h1>
+            </div>
+
+            <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
+                <CategoryCard />
+                <CategoryCard />
+                <CategoryCard />
+            </div>
+        </div>
+    );
+};
+
+export default HomeRow;
