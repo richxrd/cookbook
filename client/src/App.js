6836688e5f21ee938134a0834c60093194451987
richxrd <linrichard98@gmail.com>
2022-08-10 12:29:17 -0400

    auth page and roues

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Auth from "./pages/Auth";
 
 const App = () => {
     return (
         <div>
             <Navbar />
-            <Home />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/auth" element={<Auth />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
             <Footer />
         </div>
     );
