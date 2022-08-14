9cfcf9f50186169fe137226d42447fd2ee874c54
richxrd <linrichard98@gmail.com>
2022-08-14 10:32:40 -0400

    made categorycard into a reusable component

--- client/src/components/CategoryCard.js
+++ client/src/components/CategoryCard.js
@@ -1,20 +1,18 @@
 import React from "react";
 
-import testImg from "../assets/testPasta.PNG";
-
-const CategoryCard = () => {
+const CategoryCard = ({ title, author, image }) => {
     return (
         <div className="flex flex-col items-center relative w-full h-72">
             {/* Image */}
-            <img src={testImg} alt="" className="object-cover w-full h-5/6" />
+            <img src={image} alt="" className="object-cover w-full h-5/6" />
             {/* Title */}
 
             <div className="flex flex-col w-full">
-                <p className="text-lg truncate">
-                    why is this takign me so long wtf L pls send help weewooo
-                    peepoo
-                </p>
-                <p className="text-md text-[#ecb390]">Richard Lin</p>
+                <p className="text-lg truncate">{title}</p>
+                <div>
+                    <span className="text-md text-gray-600">By </span>
+                    <span className="text-md text-[#ecb390]">{author}</span>
+                </div>
             </div>
         </div>
     );
