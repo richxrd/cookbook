b48ff89770003ce5cb16d21c0c950f4ede5d89b9
richxrd <linrichard98@gmail.com>
2022-08-17 13:08:35 -0400

    added spacing

--- client/src/components/Ingredient.js
+++ client/src/components/Ingredient.js
@@ -2,14 +2,14 @@ import React from "react";
 
 const Ingredient = ({ name, quantity }) => {
     return (
-        <div className="flex space-x-2 my-4 items-center tracking-wide font-light">
+        <div className="flex space-x-1 my-4 items-center tracking-wide font-light">
             <input
                 type="checkbox"
                 className="bg-white hover:bg-[#ecb390] cursor-pointer 
                 w-6 h-6 border-3 border-[rgb(217,83,79)] rounded-lg 
                 checked:bg-[#96ceb4] active:bg-[#96ceb4] focus:bg-[#96ceb4] outline-none"
             />
-            {quantity && <span>{quantity}</span>}
+            {quantity && <span className="pl-2">{quantity}</span>}
             <span>{name}</span>
         </div>
     );
