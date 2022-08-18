e854bd4f0a9380edab9c4890eb6f9befa5f2bb3f
richxrd <linrichard98@gmail.com>
2022-08-18 10:32:14 -0400

    tags input

---
+++ client/src/components/TagsInput.js
@@ -0,0 +1,18 @@
+import { XIcon } from "@heroicons/react/outline";
+import React from "react";
+
+const TagsInput = ({ tag, deleteTag, index }) => {
+    return (
+        <div className="bg-[#ecb390] inline-flex justify-center items-center py-1 px-3 rounded-xl">
+            <span>{tag}</span>
+            <span
+                className="ml-2 text-red-600 cursor-pointer"
+                onClick={() => deleteTag(index)}
+            >
+                <XIcon className="w-4 h-4" />
+            </span>
+        </div>
+    );
+};
+
+export default TagsInput;
