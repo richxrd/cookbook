e22c0a0141be0dd9bc32332e24ba8de960feeb38
richxrd <linrichard98@gmail.com>
2022-08-18 10:31:50 -0400

    default cuisines

---
+++ client/src/Cuisines.js
@@ -0,0 +1,13 @@
+export const CUISINES = [
+    { value: "American", label: "American" },
+    { value: "Chinese", label: "Chinese" },
+    { value: "Cuban", label: "Cuban" },
+    { value: "Greek", label: "Greek" },
+    { value: "Indian", label: "Indian" },
+    { value: "Italian", label: "Italian" },
+    { value: "Japanese", label: "Japanese" },
+    { value: "Korean", label: "Korean" },
+    { value: "Mexican", label: "Mexican" },
+    { value: "Thai", label: "Thai" },
+    { value: "Vietnamese", label: "Vietnamese" },
+];
