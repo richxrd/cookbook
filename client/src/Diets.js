4e0c596cf705a5be74ae937d1cae1fdc5cb028cb
richxrd <linrichard98@gmail.com>
2022-08-18 10:31:58 -0400

    default diets

---
+++ client/src/Diets.js
@@ -0,0 +1,14 @@
+export const DIETS = [
+    { value: "Atkins", label: "Atkins" },
+    { value: "DASH", label: "DASH" },
+    { value: "Dukan Diet", label: "Dukan Diet" },
+    { value: "Flexitarian", label: "Flexitarian" },
+    { value: "Ketogenic", label: "Ketogenic" },
+    { value: "Low-Carb", label: "Low-Carb" },
+    { value: "Low-Fat", label: "Low-Fat" },
+    { value: "Mediterranean", label: "Mediterranean" },
+    { value: "Paleo", label: "Paleo" },
+    { value: "Vegan", label: "Vegan" },
+    { value: "Vegetarian", label: "Vegetarian" },
+    { value: "Whole 30", label: "Whole 30" },
+];
