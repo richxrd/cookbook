0d8c46066567d2e291d8d0fba28de36e42618454
richxrd <linrichard98@gmail.com>
2022-08-21 11:17:06 -0400

    server init

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import userRoutes from "./routes/users.js";
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "50mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(cors());
+
+app.use("/users", userRoutes);
+
+app.get("/", (req, res) => {
+    res.send("App is running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+    .connect(process.env.CONNECTION_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() =>
+        app.listen(PORT, () =>
+            console.log(`Server is running on port: ${PORT}`)
+        )
+    )
+    .catch((error) => console.log(error));
