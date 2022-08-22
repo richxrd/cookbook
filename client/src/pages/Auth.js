33f2afe9793f9a80a9d658141b27cdc58b017f87
richxrd <linrichard98@gmail.com>
2022-08-22 12:39:39 -0400

    changed userData to authData

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -9,7 +9,7 @@ import icon from "../assets/icon.png";
 
 const Auth = () => {
     const [user, setUser] = useState(
-        useSelector((state) => state.user.userData?.result)
+        useSelector((state) => state.user.authData?.result)
     );
 
     const dispatch = useDispatch();
@@ -55,7 +55,7 @@ const Auth = () => {
                 backgroundPosition: "center center",
             }}
         >
-            <div className="h-fit py-60 md:h-[calc(100vh-346px)] md:py-0 max-w-screen-xl mx-auto flex items-center justify-center">
+            <div className="h-fit py-60 md:h-[calc(100vh-318px)] md:py-0 max-w-screen-xl mx-auto flex items-center justify-center">
                 <div className="w-[360px] p-8 bg-white rounded-3xl drop-shadow-2xl">
                     <img src={icon} alt="/" className="w-16 mx-auto" />
                     <h1 className="uppercase text-4xl font-semibold text-center md:text-5xl">
