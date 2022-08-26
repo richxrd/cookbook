2cd6d69663812173c09da5879141ed4d6108878f
richxrd <linrichard98@gmail.com>
2022-08-26 18:39:31 -0400

    project folder restructure

---
+++ client/src/components/GlobalComponents/Footer.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="w-full bg-[#f5eedc] text-black py-y px-2">
+            <div className="max-w-screen-xl mx-auto grid grid-cols-2 md:grid-cols-4 border-b-2 border-b-yellow-300 px-6 py-8">
+                <div>
+                    <h1 className="font-bold uppercase pt-2">Diets</h1>
+                    <ul className="text-sm">
+                        <li className="py-1">Mediterranean</li>
+                        <li className="py-1">Paleo</li>
+                        <li className="py-1">Whole 30</li>
+                        <li className="py-1">Atkins</li>
+                        <li className="py-1">Vegan</li>
+                    </ul>
+                </div>
+                <div>
+                    <h1 className="font-bold uppercase pt-2">Cuisines</h1>
+                    <ul className="text-sm">
+                        <li className="py-1">American</li>
+                        <li className="py-1">Chinese</li>
+                        <li className="py-1">Indian</li>
+                        <li className="py-1">Mexican</li>
+                        <li className="py-1">Italian</li>
+                    </ul>
+                </div>
+                <div>
+                    <h6 className="font-bold uppercase pt-2">Get to Know Us</h6>
+                    <ul className="text-sm">
+                        <li className="py-1">About</li>
+                        <li className="py-1">Github</li>
+                    </ul>
+                </div>
+                <div>
+                    <h6 className="font-bold uppercase pt-2">Legal</h6>
+                    <ul className="text-sm">
+                        <li className="py-1">Privacy</li>
+                        <li className="py-1">Terms</li>
+                        <li className="py-1">Policies</li>
+                        <li className="py-1">Conditions</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="flex flex-col max-w-[1240px] px-2 py-4 mx-auto justify-between sm:flex-row text-center text-gray-800">
+                <p className="py-4 text-xs">Created by Richard Lin</p>
+                <div className="flex justify-between sm:w-[300px] pt-4 text-2xl"></div>
+            </div>
+        </div>
+    );
+};
+export default Footer;
