432933484393ff83768bdaa8549608c4f4112260
richxrd <linrichard98@gmail.com>
2022-08-27 16:49:03 -0400

    removed unused import

--- client/src/api/firebase/index.js
+++ client/src/api/firebase/index.js
@@ -1,11 +1,5 @@
 import { initializeApp } from "firebase/app";
-import {
-    getStorage,
-    ref,
-    uploadBytes,
-    listAll,
-    getDownloadURL,
-} from "firebase/storage";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { customAlphabet } from "nanoid";
 
 const firebaseConfig = {
