ab7f784652b3a884db3e5902d8d3c822d86873b0
richxrd <linrichard98@gmail.com>
2022-08-29 14:06:59 -0400

    added conditional render

--- client/src/components/Profile/ProfileRecipeCard.js
+++ client/src/components/Profile/ProfileRecipeCard.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import Rating from "../GlobalComponents/Rating";
 
 const ProfileRecipeCard = ({ recipe }) => {
     const { title, imageUrl, reviews } = recipe;
+    const auth = useSelector((state) => state.user.authData?.result);
     const navigate = useNavigate();
+
+    const isAuthUser = () => {
+        if (auth) {
+            return auth._id === recipe.authorId;
+        }
+        return false;
+    };
 
     return (
         <div className="flex flex-col m-2 aspect-[4/5] shadow-lg cursor-pointer rounded-lg relative group hover:scale-[1.02] hover:shadow-2xl transition duration-200">
@@ -24,14 +33,16 @@ const ProfileRecipeCard = ({ recipe }) => {
                 <Rating ratingsList={Object.entries(reviews)} full />
             </div>
 
-            <div className="absolute p-2 top-0 right-0 opacity-100 md:opacity-0 group-hover:opacity-100 transition duration-200">
-                <button
-                    className="w-full py-2 px-4 rounded-xl bg-[#ecb390] hover:bg-[#d9534f] bg-opacity-70"
-                    onClick={() => navigate(`/${recipe._id}/edit`)}
-                >
-                    Edit
-                </button>
-            </div>
+            {isAuthUser() && (
+                <div className="absolute p-2 top-0 right-0 opacity-100 md:opacity-0 group-hover:opacity-100 transition duration-200">
+                    <button
+                        className="w-full py-2 px-4 rounded-xl bg-[#ecb390] hover:bg-[#d9534f] bg-opacity-70"
+                        onClick={() => navigate(`/${recipe._id}/edit`)}
+                    >
+                        Edit
+                    </button>
+                </div>
+            )}
         </div>
     );
 };
