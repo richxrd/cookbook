279e334086d8714aa079a58730de6fab81268d57
richxrd <linrichard98@gmail.com>
2022-08-29 20:09:51 -0400

    recipe card

--- client/src/components/CollectionsPage/CollectionsRecipeCard.js
+++ client/src/components/CollectionsPage/CollectionsRecipeCard.js
@@ -1,12 +1,21 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import { addToCollection } from "../../api/collections";
 import { getPost } from "../../api/posts";
 import Rating from "../GlobalComponents/Rating";
 
-const CollectionsRecipeCard = ({ recipe }) => {
+const CollectionsRecipeCard = ({
+    recipe,
+    userData,
+    setCollection,
+    setRecipes,
+}) => {
     const [recipeData, setRecipeData] = useState(null);
+    const auth = useSelector((state) => state.user.authData?.result);
 
     const navigate = useNavigate();
+    const { collectionId } = useParams();
 
     useEffect(() => {
         const getData = async () => {
@@ -15,6 +24,26 @@ const CollectionsRecipeCard = ({ recipe }) => {
         };
         getData();
     }, []);
+
+    const handleDeleteFromCollection = async (e) => {
+        e.preventDefault();
+
+        const submittionForm = {
+            postId: recipeData._id,
+            collectionId: collectionId,
+        };
+
+        const data = await addToCollection(submittionForm);
+        setCollection({ ...data.result });
+        setRecipes(data.result.recipes);
+    };
+
+    const isAuthUser = () => {
+        if (auth) {
+            return auth._id === userData._id;
+        }
+        return false;
+    };
 
     const hasData = () => {
         return recipeData;
@@ -43,6 +72,16 @@ const CollectionsRecipeCard = ({ recipe }) => {
                         full
                     />
                 </div>
+                {isAuthUser() && (
+                    <div className="absolute p-2 top-0 right-0 opacity-100 md:opacity-0 group-hover:opacity-100 transition duration-200">
+                        <button
+                            className="w-full py-2 px-4 rounded-xl bg-[#ecb390] hover:bg-[#d9534f] bg-opacity-70"
+                            onClick={handleDeleteFromCollection}
+                        >
+                            Remove
+                        </button>
+                    </div>
+                )}
             </div>
         )
     );
