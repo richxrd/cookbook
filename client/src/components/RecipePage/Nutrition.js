adb59d66a4f33cb31745a5ec2ba936fe59917cfb
richxrd <linrichard98@gmail.com>
2022-08-29 20:11:21 -0400

    added measurement

--- client/src/components/RecipePage/Nutrition.js
+++ client/src/components/RecipePage/Nutrition.js
@@ -1,10 +1,26 @@
 import React from "react";
 
-const Nutrition = ({ name, value }) => {
+const Nutrition = ({ name, value, measurement }) => {
+    const getMeasurement = () => {
+        switch (measurement) {
+            case "calories":
+                return "cal";
+            case "soduium":
+                return "mg";
+            case "cholesterol":
+                return "mg";
+            default:
+                return "g";
+        }
+    };
+
     return (
         <div className="flex justify-between space-x-2 my-4 items-center tracking-wide font-light border-0 border-b-2 border-[#ecb390]">
             <span className="capitalize">{name}</span>
-            <span>{value}</span>
+            <div>
+                <span>{value}</span>
+                <span>{getMeasurement()}</span>
+            </div>
         </div>
     );
 };
