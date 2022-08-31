7f66a69e42afc8a77f69d08637562544ae8e9d2f
richxrd <linrichard98@gmail.com>
2022-08-31 16:37:08 -0400

    diets & cuisine page

---
+++ client/src/pages/CuisinesPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CuisinesPage = () => {
+    return (
+        <div className="min-h-[calc(100vh-318px)] pt-32 py-16 md:pt-32 px-6 mx-auto max-w-screen-xl">
+            CuisinesPage
+        </div>
+    );
+};
+
+export default CuisinesPage;
---
+++ client/src/pages/DietsPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DietsPage = () => {
+    return (
+        <div className="min-h-[calc(100vh-318px)] pt-32 py-16 md:pt-32 px-6 mx-auto max-w-screen-xl">
+            DietsPage
+        </div>
+    );
+};
+
+export default DietsPage;
