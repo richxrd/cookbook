bd55fc743d0a562c3cfef4f2e505ddb2ab275bb0
richxrd <linrichard98@gmail.com>
2022-08-31 16:42:28 -0400

    vercel axios url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://cookbook-server-gl5ij1chq-richxrd.vercel.app",
 });
 
 API.interceptors.request.use((req) => {
