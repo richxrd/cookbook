f3a767b0b12f81d1c6adcbc245e4d5c5cfb1a3da
richxrd <linrichard98@gmail.com>
2022-09-07 12:54:07 -0400

    add key

--- client/src/components/GlobalComponents/CategoriesColumn.js
+++ client/src/components/GlobalComponents/CategoriesColumn.js
@@ -5,7 +5,7 @@ const CategoriesColumn = ({ list }) => {
     return (
         <div className="border-0 border-green-200 text-left sm:odd:border-r-2 md:last:border-0 md:border-r-2">
             {list.map((category) => {
-                return <Category category={category} />;
+                return <Category category={category} key={category} />;
             })}
         </div>
     );
