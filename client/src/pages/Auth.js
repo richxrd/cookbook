6cdfcb5827f8e289e722617d1f45ee2acf36122e
richxrd <linrichard98@gmail.com>
2022-09-07 11:12:01 -0400

    removed middle box

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -55,13 +55,13 @@ const Auth = () => {
                 backgroundPosition: "center center",
             }}
         >
-            <div className="h-fit py-60 md:h-[calc(100vh-318px)] md:py-0 max-w-screen-xl mx-auto flex items-center justify-center">
-                <div className="w-[360px] p-8 bg-white rounded-3xl drop-shadow-2xl">
+            <div className="h-screen py-60 md:py-0 max-w-screen-xl mx-auto flex items-center justify-center">
+                <div className="p-8 drop-shadow-2xl">
                     <img src={icon} alt="/" className="w-16 mx-auto" />
-                    <h1 className="uppercase text-4xl font-semibold text-center md:text-5xl">
+                    <h1 className="uppercase text-4xl font-semibold text-center md:text-5xl drop-shadow-2xl">
                         CookBook
                     </h1>
-                    <div className="w-full py-20 text-center">
+                    <div className="w-full py-10 text-center">
                         <GoogleLogin
                             clientId="878429723573-ilbeo51e3q9loiga5ik5t833ntrvsuil.apps.googleusercontent.com"
                             render={(renderProps) => (
